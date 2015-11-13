0a13f4bbf7091a18dc5de8e3b2dda40b1005923c
Lab 38 - 01 <Lab 38 - 01@192.168.22.29>
2015-11-13 21:11:23 -0200

    Index com CSS

---
+++ GameHunter/index.php
@@ -0,0 +1,139 @@
+<!doctype   html    public "-//w3c//dtd xhtml 1.0 strict//en"
+"http://www.w3.org/TR/XHTML1/DTD/XHTML1-STRICT.DTD">
+<html lang="pt-br">
+    <head>
+        <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+            <link href="CssMain/mainStyle.css" rel="stylesheet" type="text/plain"/>
+        <title>GameHunter</title>
+    </head>
+    <body>
+        <main>
+            <nav id="menuGrande">
+                <div id="logoMenu">
+                    <img class="LogoImg" src="Image/Logo.jpg" alt="Logo GameHunter" />
+                </div>
+                <div id="menuPequeno">
+                    <ul>
+                        <a href="index.php"><li class="liMenu"><span class="spanMenu">Home</span></li></a>
+                        <a href="index.php"><li class="liMenu"><span class="spanMenu">Meus Jogos</span></li></a>
+                        <a href="index.php"><li class="liMenu"><span class="spanMenu">Amigos</span></li></a>
+                        <a href="index.php"><li class="liMenu"><span class="spanMenu">Localizar</span></li></a>
+                        <a href="index.php"><li class="liMenu"><span class="spanMenu">Sair</span></li></a>
+                    </ul>
+                </div>
+            </nav>
+            <div id="center">
+                    <br />
+                <div id="top">
+                    <h1 class="titleA">Bem-Vindo!</h1>
+                        <br />
+                    <p class="pA">Para ter total acesso, por favor faça<br />
+                       o <a href="" class="linkA">login</a> com sua conta <br /> 
+                       do Facebook.
+                    </p>
+                </div>
+                <div id="bot">
+                    <div id="esquerda">
+                        <h3 class="titleB">Notícias</h3>
+                        <div id="esquerdaDentro">
+                            <div class="lastNewsA">
+                                <div class="lastNewsImg">
+                                    <img class="lastNewsImgA" src="Image/Logo.jpg" />
+                                </div>
+                                <div class="lastNewsTxt">
+                                    <h1 class="titleC">mmmmmmmmmmmmmmm</h1>
+                                    <p>
+                                        mmmmmmmmmmmmmmmm<br/>
+                                        mmmmmmmmmmmmmmmm<br/>
+                                        mmmmmmmmmmmmmmmm<br/>
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="lastNewsA">
+                                <div class="lastNewsImg">
+                                    <img class="lastNewsImgA" src="Image/Logo.jpg" />
+                                </div>
+                                <div class="lastNewsTxt">
+                                    <h1 class="titleC">mmmmmmmmmmmmmmm</h1>
+                                    <p>
+                                        mmmmmmmmmmmmmmmm<br/>
+                                        mmmmmmmmmmmmmmmm<br/>
+                                        mmmmmmmmmmmmmmmm<br/>
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="lastNewsA">
+                                <div class="lastNewsImg">
+                                    <img class="lastNewsImgA" src="Image/Logo.jpg" />
+                                </div>
+                                <div class="lastNewsTxt">
+                                    <h1 class="titleC">mmmmmmmmmmmmmmm</h1>
+                                    <p>
+                                        mmmmmmmmmmmmmmmm<br/>
+                                        mmmmmmmmmmmmmmmm<br/>
+                                        mmmmmmmmmmmmmmmm<br/>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <center>
+                            <br />
+                                <a class="linkB" href="#" >Ver todos</a>
+                        </center>
+                    </div>
+                    <div id="direita">
+                        <h3 class="titleB">Ultimos Lançamentos</h3>
+                        <div id="direitaDentro">
+                            <div class="lastNewsA">
+                                <div class="lastNewsImg">
+                                    <img class="lastNewsImgA" src="Image/Logo.jpg" />
+                                </div>
+                                <div class="lastNewsTxt">
+                                    <h1 class="titleC">mmmmmmmmmmmmmmm</h1>
+                                    <p>
+                                        mmmmmmmmmmmmmmmm<br/>
+                                        mmmmmmmmmmmmmmmm<br/>
+                                        mmmmmmmmmmmmmmmm<br/>
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="lastNewsA">
+                                <div class="lastNewsImg">
+                                    <img class="lastNewsImgA" src="Image/Logo.jpg" />
+                                </div>
+                                <div class="lastNewsTxt">
+                                    <h1 class="titleC">mmmmmmmmmmmmmmm</h1>
+                                    <p>
+                                        mmmmmmmmmmmmmmmm<br/>
+                                        mmmmmmmmmmmmmmmm<br/>
+                                        mmmmmmmmmmmmmmmm<br/>
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="lastNewsA">
+                                <div class="lastNewsImg">
+                                    <img class="lastNewsImgA" src="Image/Logo.jpg" />
+                                </div>
+                                <div class="lastNewsTxt">
+                                    <h1 class="titleC">mmmmmmmmmmmmmmm</h1>
+                                    <p>
+                                        mmmmmmmmmmmmmmmm<br/>
+                                        mmmmmmmmmmmmmmmm<br/>
+                                        mmmmmmmmmmmmmmmm<br/>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <center>
+                            <br />
+                                <a class="linkB" href="#" >Ver todos</a>
+                        </center>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <footer>
+            <p class="pFooterA">Todos os direitos reservados a GamerHunter</p>
+        </footer>
+    </body>
+</html>
---
+++ GameHunter/index2.php
@@ -0,0 +1,41 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Game Hunter | Home</title>
+		<link rel="icon" href="imagens/ico.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="imagens/icon.ico" type="image/x-icon" />
+    </head>
+    <body>
+        <ul>
+            <li><a href="home.php">Home</a></li>
+            <li><a href="meusj.php">Meus Jogos</a></li>
+            <li><a href="amigos.php">Amigos</a></li>
+            <li><a href="perfil.php">Perfil</a></li>
+        </ul>
+        <h1>Bem-vindo!</h1>
+        <h2>Para ter total acesso, por favor faça o <a href="login.php">login</a> com a sua conta do facebook.</h2>
+        <ul>
+            <li><h3>Noticias</h3></li>
+            <li>Nova expansao World of Warcraf Legion e anunciada na BlizzCon.</li>
+            <li>Overwatch ganha edicoes especiais e versoes para PS4 e Xbox One.</li>
+            <li>StarCraft 2: Nova Covert Ops continuara a historia do game em DLC.</li>
+            <li><h5><a href="noticias.php">Ver todas</a></h5></li>
+        </ul>
+        <ul>
+            <li><h3>Ultimos lancamentos</h3></li>
+            <li>Call Of Duty:Black Ops III 
+                <p>Plataformas: Xbox One, PS4, Xbox 360, PS3 e PC</p>
+                <p>Data de lancamento: 06/11/2015</p>
+            </li>
+            <li>Need for Speed 2015 
+                <p>Plataformas: Xbox One, PS4 e PC</p>
+                <p>Data de lancamento: 03/11/2015</p>
+            </li>
+            <li>FIFA 16
+                <p>Plataformas: Xbox One, PS4, Xbox 360, PS3 e PC</p>
+                <p>Data de lancamento: 22/09/2015</p>
+            </li>
+            <li><a href="lanc.php"><h5>Ver todos</h5></a></li>
+        </ul>
+    </body>
+</html>
